3a11d9998c94bb542a123b55051b91c7718e0988
Quakken <69021181+Quakken@users.noreply.github.com>
2025-09-05 07:24:16 -0700

    [1.2/4.0] Time utilities

--- src/foo.cpp
+++
@@ -1,9 +0,0 @@
-#include "foo.hpp"
-
-int foo::bar::baz() noexcept {
-    #if defined(JUNCO_BUILD_DEBUG)
-        return -1;
-    #elif defined(JUNCO_BUILD_RELEASE)
-        return 1;
-    #endif
-}
